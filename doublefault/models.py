6a94b33e5ae2d26a4e71ee7565b74eaf5087ed70
Lawrence Lee <lawrence.lee@invitae.com>
2017-07-19 23:27:35 -0700

    more fixes

--- doublefault/models.py
+++ doublefault/models.py
@@ -9,25 +9,25 @@ class Post(models.Model):
     owner = models.ForeignKey(User)
     upvotes = models.ManyToManyField(User, related_name='upvoted_posts')
     downvotes = models.ManyToManyField(User, related_name='downvoted_posts')
-    parent_post = models.ForeignKey("self", related_name='child_posts')
+    parent_post = models.ForeignKey('self', related_name='child_posts')
 
     @property
     def score(self):
         return self.upvotes.count() - self.downvotes.count()
 
-class Question(Post):
-    tags = models.ManyToManyField("Tag", related_name='questions')
-    accepted_answer = models.OneToOneField("Answer", related_name='accepted')
+class QuestionPost(Post):
+    tags = models.ManyToManyField('Tag', related_name='questions')
+    accepted_answer = models.OneToOneField('AnswerPost', related_name='accepted')
     views = models.PositiveIntegerField()
     parent_post = None
 
 
-class Answer(Post):
-    question = models.ForeignKey("Question", related_name='answers')
+class AnswerPost(Post):
+    question = models.ForeignKey('QuestionPost', related_name='answers')
 
 
 class Tag(models.Model):
-    name = models.CharField(unique=True)
+    name = models.CharField(unique=True, max_length=128)
     users = models.ManyToManyField(User, related_name='following')
 
     def save(self, *args, **kwargs):
