a61bd9d79d73e739e6be6476a2e5f43b3e5bee8e
Lawrence Lee <lawrence.lee@invitae.com>
2017-07-21 00:32:18 -0700

    remove oauth keys

--- doublefault/settings.py
+++ doublefault/settings.py
@@ -111,9 +111,6 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
 ]
 
-SOCIAL_AUTH_GOOGLE_OAUTH_KEY = '457548298202-2rfqrsbcpcbqnap3gve09dm1ubmpo8n1.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH_SECRET = 'kJK7fzhEiod4QEH-1n-cYfMI'
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -132,3 +129,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH_KEY = os.env['GOOGLE_OAUTH_KEY']
+SOCIAL_AUTH_GOOGLE_OAUTH_SECRET = os.env['GOOGLE_OAUTH_SECRET']
