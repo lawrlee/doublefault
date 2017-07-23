ddf3f89b4e44338bb2fd13947b883568771ebe72
Lawrence Lee <lawrence.lee@invitae.com>
2017-07-23 00:31:58 -0700

    add home view

--- doublefault/views.py
+++ doublefault/views.py
@@ -1,1 +1,7 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
