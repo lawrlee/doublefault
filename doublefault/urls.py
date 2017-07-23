c0bcdfc6028c6dab4a0c712654fd68848080b59d
Lawrence Lee <lawrence.lee@invitae.com>
2017-07-23 00:31:37 -0700

    add home url

--- doublefault/urls.py
+++ doublefault/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from doublefault import views
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^oauth/', include('social_django.urls', namespace='social'))
+    url(r'^oauth2/', include('social_django.urls', namespace='social'))
 ]
