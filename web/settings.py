014e496d6b0f1ce766497e364ec5f7624d90bc30
Lawrence Lee <lawrence.lee@invitae.com>
2017-07-28 13:06:52 -0700

    add logging settings

--- web/settings.py
+++ web/settings.py
@@ -27,6 +27,25 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'graphql.execution.executor': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        }
+    },
+}
 
 # Application definition
 
