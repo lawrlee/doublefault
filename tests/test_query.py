49bc1c17964ca64c0e04526a49018c103206e074
Lawrence Lee <lawrence.lee@invitae.com>
2017-09-16 11:11:21 -0700

    update tests for non relay queries

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,62 +3,54 @@ import pytest
 FULL_QUERY = '''
 {
   allQuestions {
-    edges {
-      node {
+    id
+    text
+    created
+    modified
+    score
+    views
+    tags {
+      id
+    }
+    owner {
+      id
+      username
+    }
+    upvotes {
+      id
+    }
+    downvotes {
+      id
+    }
+    acceptedAnswer {
+      id
+    }
+    comments {
+      id
+      text
+      owner {
+        id
+        username
+      }
+    }
+    answers {
+      id
+      text
+      accepted {
+        id
+      }
+      created
+      modified
+      owner {
+        id
+        username
+      }
+      comments {
         id
         text
-        created
-        modified
-        score
-        views
-        tags {
-          id
-        }
         owner {
           id
           username
-        }
-        upvotes {
-          id
-        }
-        downvotes {
-          id
-        }
-        acceptedAnswer {
-          id
-        }
-        comments {
-          id
-          text
-          owner {
-            id
-            username
-          }
-        }
-        answers {
-          edges {
-            node {
-              id
-              text
-              accepted {
-                id
-              }
-              created
-              modified
-              owner {
-                id
-                username
-              }
-              comments {
-                id
-                text
-                owner {
-                  id
-                  username
-                }
-              }
-            }
-          }
         }
       }
     }
@@ -69,6 +61,7 @@ FULL_QUERY = '''
     }
   }
 }
+
 '''
 
 
@@ -76,26 +69,22 @@ def _get_all_query(query):
     return '''
 {{
   {query} {{
-    edges {{
-      node {{
-        id
-      }}
-    }}
-  }} 
+    id
+  }}
 }}
 '''.format(query=query)
 
 
-def _get_single_query(query, relay_id):
+def _get_single_query(query, id):
     return '''
 {{ 
-  {query}(id: "{relay_id}") {{
+  {query}(id: "{id}") {{
     id
     text
   }}
 }}
     '''.format(query=query,
-               relay_id=relay_id)
+               id=id)
 
 
 LIST_QUERIES = ['allQuestions',
@@ -121,6 +110,6 @@ def test_get_all(client, snapshot, query):
 @pytest.mark.parametrize('list_query,single_query', zip(LIST_QUERIES, SINGLE_QUERIES))
 def test_get_single(client, snapshot, list_query, single_query):
     all_objects = client.execute(_get_all_query(list_query))
-    relay_id = all_objects['data'][list_query]['edges'][0]['node']['id']
+    relay_id = all_objects['data'][list_query][0]['id']
     result = client.execute(_get_single_query(single_query, relay_id))
     snapshot.assert_match(result)
