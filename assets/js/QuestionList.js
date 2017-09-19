9ccec9c66e7611ebff89d90d1fa731f4be1c3944
Lawrence Lee <lawrence.lee@invitae.com>
2017-09-19 00:27:11 -0700

    working layout with tables

--- assets/js/QuestionList.js
+++ assets/js/QuestionList.js
@@ -1,46 +1,115 @@
-import {
-  createFragmentContainer,
-  graphql
-} from 'react-relay'
+import {graphql, compose, gql} from 'react-apollo'
 import React, {Component} from 'react'
-import Question from './Question'
+import {Icon, Spinner, Tag} from "@blueprintjs/core";
+import moment from 'moment';
 
-// const mockQuestionData = [
-//   {
-//     node: {
-//       id: "1",
-//       text: "Howdy Partner"
-//     }
-//   },
-//   {
-//     node: {
-//       id: "2",
-//       text: "Ice Cream!"
-//     }
-//   }
-// ]
+const allQuestions = gql`
+  query allQuestions {
+    allQuestions {
+      id
+      title
+      text
+      created
+      owner { 
+        getFullName
+      }
+      score
+      answers {
+        id
+      }
+      views
+      tags {
+        id
+        name
+      }
+    }
+  }
+`
 
 class QuestionList extends Component {
 
   render() {
-    const questions = this.props.questions.allQuestions;
+    if (this.props.allQuestionsQuery.loading) {
+      return <Spinner className="pt-large" intent="primary"/>
+    }
+
+    const questions = this.props.allQuestionsQuery.allQuestions;
+
     return (
-      <div className="alert alert-success" role="alert">
-        {questions.edges.map(({node}) =>
-          <Question key={node.id} question={node}/>
-        )}
-      </div>
+      <main>
+        <left></left>
+        <content>
+          {questions.map((question) =>
+            <QuestionItem key={question.id} question={question}/>
+          )}
+        </content>
+        <right></right>
+      </main>
     )
   }
 }
 
+class QuestionItem extends Component {
 
-export default createFragmentContainer(QuestionList, graphql`
-  fragment QuestionList_questions on QuestionNodeConnection {
-    edges {
-      node {
-        ...Question_question
-      }
-    }
+  render() {
+    const question = this.props.question;
+
+    return (
+      <table style={{tableLayout: 'fixed'}}>
+        <tbody>
+        <tr>
+          <td style={{width: '260px'}}>
+            <div style={{display: 'flex'}}>
+              <div className="pt-card" style={{textAlign: 'center'}}>
+                <h5>{question.score}</h5>
+                <span className="pt-text-muted">Votes</span>
+              </div>
+              <div className="pt-card" style={{textAlign: 'center'}}>
+                <h5>{question.answers.length}</h5>
+                <span className="pt-text-muted">Answers</span>
+              </div>
+              <div className="pt-card" style={{textAlign: 'center'}}>
+                <h5>{question.views}</h5>
+                <span className="pt-text-muted">Views</span>
+              </div>
+            </div>
+          </td>
+          <td style={{width: '100%'}}>
+            <table style={{width: '100%'}}>
+              <tbody>
+              <tr>
+                <td><h5>{question.title}</h5></td>
+              </tr>
+              <tr>
+                <td>
+                  <blockquote className="pt-text-overflow-ellipsis">
+                    {question.text}
+                  </blockquote>
+                </td>
+              </tr>
+              <tr>
+                <td>{question.tags.map((tag) =>
+                  <Tag key={tag.id} className="pt-tag pt-minimal pt-intent-primary">{tag.name}</Tag>)
+                }
+                </td>
+              </tr>
+              <tr>
+                <td style={{textAlign: 'right'}}>
+                  asked {moment(question.created).fromNow()} - {question.owner.getFullName}&nbsp;
+                  <Icon iconName="pt-icon-person"/>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    )
   }
-`)
+
+}
+
+export default compose(
+  graphql(allQuestions, {name: 'allQuestionsQuery'})
+)(QuestionList)
